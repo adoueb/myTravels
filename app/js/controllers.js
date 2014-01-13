b650dd6d8c0c495ac3c55bcb53bd5684b5a20cec
adoueb <adoueb@club-internet.fr>
2014-01-13 19:03:21 +0100

    change readme

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 angular.module('travelApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+  controller('TravelListCtrl', function($scope, $http) {
+	  $http.get('travels/travels.json').success(function(data) {
+	    $scope.travels = data;
+	    if ($scope.travels.length > 0) {
+	    	$scope.selectedTravel =  $scope.travels[0];
+	    } else {
+	    	$scope.selectedTravel = undefined;
+	    }
+	  });
+	});
--- app/js/filters.js
+++ app/js/filters.js
@@ -6,5 +6,21 @@ angular.module('travelApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
-    }
+    };
+  }]).
+  filter('excludeSelectedTravelFilter', [function() {
+	  return function(travels, selectedTravel) {
+		  if (!angular.isUndefined(travels) && !angular.isUndefined(selectedTravel)) {
+			  var tempTravels = [];
+			  angular.forEach(travels, function(travel) {
+				  if (!angular.equals(travel.id, selectedTravel.id)) {
+					  tempTravels.push(travel);
+                  }
+              });
+			  return tempTravels;
+		} else {
+			  return travels;
+		}
+	  };
   }]);
+
