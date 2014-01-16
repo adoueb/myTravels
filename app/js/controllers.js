6deb2077ba23f2b8949c3ef9cda5a58fad53cb4a
adoueb <agnes.doueb@gmail.com>
2014-01-16 15:34:58 +0100

    add some fixes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,19 @@
 /* Controllers */
 
 angular.module('travelApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+  controller('TravelListCtrl', function($scope, $http) {
+	  $http.get('travels/travels.json').success(function(data) {
+		// Travel list.
+	    $scope.travels = data;
+	    
+	    // Selected travel.
+	    if ($scope.travels.length > 0) {
+	    	$scope.selectedTravel =  $scope.travels[0];
+	    } else {
+	    	$scope.selectedTravel = undefined;
+	    }
+	    
+	    // Order.
+	    $scope.orderProp = 'year';
+	  });
+	});
