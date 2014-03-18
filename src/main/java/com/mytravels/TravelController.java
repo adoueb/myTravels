cfc29e96688ab1d544978f4b3ef785bfe956d96b
adoueb <agnes.doueb@gmail.com>
2014-03-18 17:29:34 +0100

    use TravelRepository in webapp

--- src/main/java/com/mytravels/TravelController.java
+++ src/main/java/com/mytravels/TravelController.java
@@ -11,14 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.collect.Lists;
 import com.mytravels.persistence.domain.Stop;
 import com.mytravels.persistence.domain.Travel;
+import com.mytravels.persistence.repository.TravelRepository;
 import com.mytravels.persistence.repository.TravelRepositoryCustom;
+
+
 
 @Controller
 public class TravelController {
+	
+	public TravelRepository getTravelRepository() {
+		
+		ConfigurableApplicationContext context = null;
+		// use @Configuration using Java:
+        context = new ClassPathXmlApplicationContext("META-INF/spring/mytravelsContext.xml");
+  	
+        return context.getBean(TravelRepository.class);
+	}
 
-	public TravelRepositoryCustom getTravelRepository() {
+	public TravelRepositoryCustom getTravelRepositoryCustom() {
 		
 		ConfigurableApplicationContext context = null;
 		// use @Configuration using Java:
@@ -32,44 +45,45 @@ public class TravelController {
 
 	@RequestMapping(value="/travels", method = RequestMethod.GET )
     public @ResponseBody List<Travel> getTravels() {
-        List<Travel> travels = getTravelRepository().getTravels();
+		List<Travel> travels = Lists.newArrayList(getTravelRepository().findAll());		
+        //List<Travel> travels = getTravelRepositoryCustom().getTravels();
         System.out.println("getTravels DONE!");
         return travels;
     }
 	
 	@RequestMapping("travels/{id}")
 	public @ResponseBody Travel getTravelById(@PathVariable String id) {
-		Travel travel = getTravelRepository().findById(id);
+		Travel travel = getTravelRepositoryCustom().findById(id);
         System.out.println("getTravelById DONE!");
 		return travel;
 	}
 	
 	@RequestMapping(value="/travels", method = RequestMethod.POST )
 	public @ResponseBody List<Travel> createTravel(@RequestBody Travel travel) {
-		getTravelRepository().addTravel(travel);
+		getTravelRepository().save(travel);
         System.out.println("createTravel DONE!");
-		return getTravelRepository().getTravels();
+		return Lists.newArrayList(getTravelRepository().findAll());
 	}
 	
 	@RequestMapping(value="/travels/{id}", method = RequestMethod.PUT )
 	public @ResponseBody List<Travel> updateTravel(@PathVariable String id, @RequestBody Travel travel) {
-		getTravelRepository().updateTravel(travel);
+		getTravelRepository().save(travel);
         System.out.println("updateTravel DONE!");
-		return getTravelRepository().getTravels();
+		return Lists.newArrayList(getTravelRepository().findAll());
 	}
 	
 	@RequestMapping(value="/travels/{id}", method = RequestMethod.DELETE )
 	public @ResponseBody List<Travel> deleteTravel(@PathVariable String id) {
         System.out.println("deleteTravel " + id);
-		getTravelRepository().deleteTravelById(id);
+		getTravelRepository().delete(id);
         System.out.println("deleteTravel DONE!");
-		return getTravelRepository().getTravels();
+		return Lists.newArrayList(getTravelRepository().findAll());
 	}
 	
 	@RequestMapping(value="/stops", method = RequestMethod.PUT )
 	public @ResponseBody List<Travel> updateStop(@RequestBody Stop stop) {
-		getTravelRepository().updateStop(stop);
+		getTravelRepositoryCustom().updateStop(stop);
         System.out.println("updateStop DONE!");
-		return getTravelRepository().getTravels();
+		return getTravelRepositoryCustom().getTravels();
 	}
 }
