b7be0ae338862210f6c1127f8f491243768b8d51
adoueb <agnes.doueb@gmail.com>
2014-07-15 17:18:43 +0200

    refactor(unit tests): add Jasmine unit tests, run them in maven clean up useless files for unit tests

---
+++ WebContent/app/js/front/travels/test/controllersSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('travels controllers tests', function(){  
+
+  it('should ....', function() {  
+	  expect(true).toEqual(true);
+  });
+});
