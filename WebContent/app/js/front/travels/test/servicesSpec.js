4f82a3e39c49db3c52df15d91ecec6815a78c220
adoueb <agnes.doueb@gmail.com>
2014-07-17 21:56:24 +0200

    refactor(unit tests): test AddTravelService, UpdateTravelService, DeleteTravelService

--- WebContent/app/js/front/travels/test/servicesSpec.js
+++ WebContent/app/js/front/travels/test/servicesSpec.js
@@ -4,6 +4,12 @@
 
 describe('travels services tests', function() {
 
+	describe('TravelService tests', function() {
+		  it('should ....', function() {  
+		  });
+	});
+		
+
 	describe('AddTravelService tests', function() {
 		
 		// Load the travels-services module, which contains the service
@@ -25,6 +31,56 @@ describe('travels services tests', function() {
 		it('should show error when required', function() {
 			AddTravelService.setShowAddTravelError(true);
 			expect(AddTravelService.getShowAddTravelError()).toBeTruthy();
+		});
+	});
+	
+
+	describe('UpdateTravelService tests', function() {
+		
+		// Load the travels-services module, which contains the service
+		beforeEach(module('travels-services'));
+	 
+		var UpdateTravelService, $log;
+		beforeEach(inject(function(_UpdateTravelService_,_$log_) {
+			UpdateTravelService = _UpdateTravelService_;
+			$log = _$log_;
+		}));
+
+		// By default, no error to show.
+		it('should set no error by default', function() {
+			expect(UpdateTravelService).toBeDefined();
+			expect(UpdateTravelService.getShowUpdateTravelError()).toBeFalsy();
+		});
+
+		// Show error.
+		it('should show error when required', function() {
+			UpdateTravelService.setShowUpdateTravelError(true);
+			expect(UpdateTravelService.getShowUpdateTravelError()).toBeTruthy();
+		});
+	});
+
+
+	describe('DeleteTravelService tests', function() {
+		
+		// Load the travels-services module, which contains the service
+		beforeEach(module('travels-services'));
+	 
+		var DeleteTravelService, $log;
+		beforeEach(inject(function(_DeleteTravelService_,_$log_) {
+			DeleteTravelService = _DeleteTravelService_;
+			$log = _$log_;
+		}));
+
+		// By default, no error to show.
+		it('should set no error by default', function() {
+			expect(DeleteTravelService).toBeDefined();
+			expect(DeleteTravelService.getShowDeleteTravelError()).toBeFalsy();
+		});
+
+		// Show error.
+		it('should show error when required', function() {
+			DeleteTravelService.setShowDeleteTravelError(true);
+			expect(DeleteTravelService.getShowDeleteTravelError()).toBeTruthy();
 		});
 	});
 });
